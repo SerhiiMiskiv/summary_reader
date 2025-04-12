bd04b67705ec01e7245be1856f57c1d87fca2181
Serhii Miskiv <serjjedi@gmail.com>
2025-04-12 16:40:04 +0100

    Replace macOS project with iOS version

---
+++ summary_reader/summary_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  summary_readerApp.swift
+//  summary_reader
+//
+//  Created by Serhii Miskiv on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct summary_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ summary_readerUITests/summary_readerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  summary_readerUITestsLaunchTests.swift
+//  summary_readerUITests
+//
+//  Created by Serhii Miskiv on 12.04.2025.
+//
+
+import XCTest
+
+final class summary_readerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
